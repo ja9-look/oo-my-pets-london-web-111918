7ac59467846821197d9e7c8ad9e21c08c1e0b39b
Janine Luk <janineluk95@gmail.com>
2018-11-24 11:18:04 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -47,13 +47,11 @@ class Owner
   end
   
   def walk_dogs
-    @pets[:dogs].each {|dog|
-    dog.mood = "happy"}
+    @pets[:dogs].each {|dog| dog.mood = "happy"}
   end
   
   def play_with_cats
-    @pets[:cats].each {|cat|
-    cat.mood = "happy"}
+    @pets[:cats].each {|cat| cat.mood = "happy"}
   end
   
 end
